83e541dbd6dccbfd4350cc35da949b232409c14b
Pavlov Konstantin <thresh@immo.ru>
2008-02-05 16:24:33 +0300

    Merge back the files.

--- yabeda.rb
+++ yabeda.rb
@@ -1,12 +1,112 @@
 #!/usr/bin/env ruby
 
+# vim: set fdm=marker :
+
 require 'pp'
 require 'tmail'
 
-require 'yabeda-logging.rb'
-require 'yabeda-config.rb'
-require 'yabeda-common.rb'
+CONFIGFILE = '/etc/yabeda/yabeda.conf'
+STATEFILE = '/var/lib/yabeda/state'
 
+defaults = {
+    'debug'                     =>  true,
+    'disallowed_proc'           =>  '.+\/0$',
+    'hostname_allowedregex'     =>  '.+',
+    'hostname_suffix'           =>  '.cryo.net.ru',
+    'time_format'               =>  '=%H:%M:%S %d-%m-%Y',
+    'message_format'            =>  '%s: CT %s on %s: %s changed from %s to %s.',
+    'enabled_modules'           =>  'console',
+    'mail_from'                 =>  'Yabeda OVZ watcher <yabeda@cryo.net.ru>',
+    'mail_to'                   =>  'pavlov.konstantin@gmail.com',
+    'mail_subject'              =>  'Problem detected!'
+}
+
+# {{{ Common IO functions
+
+def readFile( file )
+    if ( FileTest.exists?( file ) ) and ( FileTest.readable_real?( file ) ) then
+        contents = File.read( file )
+        return contents
+    end
+    msgDbg( "Failed to open and/or read file #{file}" )
+    return false
+end
+
+def writeFile( file, contents )
+    filedescr = File.open( file, 'w+' )
+    contents.each { |arr|
+        line = arr.join(' ')
+        filedescr.write( line + "\n" )
+    }
+    filedescr.close
+    return true
+end
+
+# }}}
+
+# {{{ Configuration functions
+def getConfig( file )
+
+    configfile = FileTest.exists?( file ) ? file : './yabeda.conf'
+    configfile = FileTest.exists?( configfile ) ? configfile : false
+
+    if configfile == false then
+        puts 'Error opening config file.'
+        exit 0
+    end
+
+    contents = readFile( configfile )
+    opt = Hash.new
+
+    if contents then
+        contents.each do |line|
+            line.chomp!
+            ( key, arg ) = line.split( /=/, 2 )
+            opt[ key ] = arg
+        end
+        return opt
+    end
+    return false
+end
+
+def getParameter( param )
+    real_param = CONFIG[ param ].nil? ? DEFAULTS[ param ] : CONFIG[ param ]
+    return real_param
+end
+
+# }}}
+
+# {{{ Debug functions
+def msgDbg( error )
+    debug = getParameter('debug')
+
+    error = getTime() + ': ' + error
+
+    debug and puts error
+end
+
+# }}}
+
+# {{{ Helper functions
+
+def getHostname()
+    hostname_allowedregex = getParameter('hostname_allowedregex')
+    hostname_suffix = getParameter('hostname_suffix')
+
+    hostname = ENV['HOSTNAME'].to_s
+    if !hostname.match( /#{hostname_allowedregex}/ ) then
+        hostname += hostname_suffix
+    end
+    return hostname
+end
+
+def getTime()
+        return Time.now.to_i.to_s
+end
+
+# }}}
+
+# {{{ Acquriing data
 def validateData( data )
     unless data == false
         validatedContent = Array.new
@@ -79,6 +179,10 @@ def compareData( oldData, currentData )
     return results
 end
 
+# }}}
+
+# {{{ Alert generation
+
 def alertDispatcher( results )
     enabled_modules = getParameter('enabled_modules')
 
@@ -141,12 +245,18 @@ def doAlert( mod, results )
     end
 end
 
+# }}}
+
+# {{{ Main program
+
 # typical workflow:
 # getProcPaths() -> getResource() -> currentData
 # readFile(state) -> validateData() -> oldData
 # compareData -> results
 # alertDispatcher(results) -> doAlert()
 # writeFile(state, currentdata)
+
+CONFIG = getConfig( CONFIGFILE )
 
 oldData = validateData( readFile( STATEFILE ) )
 
@@ -160,3 +270,4 @@ end
 #if currentData then
 #    writeFile( STATEFILE, currentData)
 #end
+# }}}
