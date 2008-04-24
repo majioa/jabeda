8a8d64e10502e85d87f748302be119a75d0bad7f
Pavlov Konstantin <thresh@immo.ru>
2008-04-24 22:59:38 +0400

    Use YAML to parse config file. Added some debug.

--- yabeda.rb
+++ yabeda.rb
@@ -6,26 +6,26 @@ require 'pp'
 require 'tmail'
 require 'socket'
 require 'dbi'
+require 'yaml'
 
-CONFIGFILE = '/etc/yabeda/yabeda.conf'
-STATEFILE = '/var/lib/yabeda/state'
-
-DEFAULTS = {
-    'debug'                     =>  true,
-    'disallowed_proc'           =>  '.+\/0$',
-    'hostname_allowedregex'     =>  '.+',
-    'hostname_suffix'           =>  '.cryo.net.ru',
-    'time_format'               =>  '=%H:%M:%S %d-%m-%Y',
-    'message_format'            =>  '%s: CT %s on %s: %s failcnt changed from %s to %s.',
-    'enabled_modules'           =>  'console',
-    'mail_from'                 =>  'Yabeda OVZ watcher <insert@lame-name.here>',
-    'mail_to'                   =>  'foobar@domain.tld',
-    'subject_format'            =>  'VPS%d: %s failcnt -> %s!',
-    'mysql_host'                =>  'localhost',
-    'mysql_db'                  =>  'yabeda',
-    'mysql_user'                =>  'yabeda',
-    'mysql_password'            =>  'yabeda',
-    'mysql_table'               =>  'stats'
+$config = {
+    :configfile                =>  "/etc/yabeda/yabeda.conf",
+    :statefile                 =>  "/var/lib/yabeda/state",
+    :debug                     =>  true,
+    :disallowed_proc           =>  ".+\/0$",
+    :hostname_allowedregex     =>  ".+",
+    :hostname_suffix           =>  ".cryo.net.ru",
+    :time_format               =>  "%H:%M:%S %d-%m-%Y",
+    :message_format            =>  "%s: CT %s on %s: %s failcnt changed from %s to %s.",
+    :enabled_modules           =>  "console",
+    :mail_from                 =>  "Yabeda OVZ watcher <insert@lame-name.here>",
+    :mail_to                   =>  "foobar@domain.tld",
+    :subject_format            =>  "VPS%d: %s failcnt -> %s!",
+    :mysql_host                =>  "localhost",
+    :mysql_db                  =>  "yabeda",
+    :mysql_user                =>  "yabeda",
+    :mysql_password            =>  "yabeda",
+    :mysql_table               =>  "stats"
 }
 
 # {{{ Common IO functions
@@ -57,39 +57,21 @@ def getConfig( file )
     configfile = FileTest.exists?( file ) ? file : './yabeda.conf'
     configfile = FileTest.exists?( configfile ) ? configfile : false
 
-    if configfile == false then
+    if !configfile then
         puts 'Error opening config file.'
         exit 0
     end
 
-    contents = readFile( configfile )
-    opt = Hash.new
-
-    if contents then
-        contents.each do |line|
-            line.chomp!
-            ( key, arg ) = line.split( /=/, 2 )
-            opt[ key ] = arg
-        end
-        return opt
-    end
-    return false
-end
-
-def getParameter( param )
-    real_param = CONFIG[ param ].nil? ? DEFAULTS[ param ] : CONFIG[ param ]
-    return real_param
+    $config.merge!(YAML.load(File.open(configfile)))
 end
 
 # }}}
 
 # {{{ Debug functions
 def msgDbg( error )
-    debug = getParameter('debug')
-
     error = getTime() + ': ' + error
 
-    debug and puts error
+    $config[:debug] and puts error
 end
 
 # }}}
@@ -97,12 +79,9 @@ end
 # {{{ Helper functions
 
 def getHostname()
-    hostname_allowedregex = getParameter('hostname_allowedregex')
-    hostname_suffix = getParameter('hostname_suffix')
-
     hostname = Socket.gethostname
-    if !hostname.match( /#{hostname_allowedregex}/ ) then
-        hostname += hostname_suffix
+    if !hostname.match( /#{$config[:hostname_allowedregex]}/ ) then
+        hostname += $config[:hostname_suffix]
     end
     return hostname
 end
@@ -116,12 +95,7 @@ end
 # {{{ MySQL functions
 
 def connectSql()
-    hostname = getParameter('mysql_host')
-    database = getParameter('mysql_db')
-    user = getParameter('mysql_user')
-    password = getParameter('mysql_password')
-
-    dbh = DBI.connect("DBI:Mysql:database=#{database};host=#{hostname}", user, password)
+    dbh = DBI.connect("DBI:Mysql:database=#{$config[:mysql_db]};host=#{$config[:mysql_host]}", $config[:mysql_user], $config[:mysql_password])
     return dbh
 end
 
@@ -146,12 +120,11 @@ end
 
 def getProcPaths()
     returnpaths = Array.new
-    disregex = getParameter('disallowed_proc')
 
     paths = Dir["/proc/bc/*"].sort
 	if paths then
         paths.each { |path|
-            if FileTest.directory?( path ) and !path.match( /#{disregex}/ ) then
+            if FileTest.directory?( path ) and !path.match( /#{$config[:disregex]}/ ) then
 	            returnpaths << path
             end
 	    }
@@ -198,7 +171,13 @@ def compareData( oldData, currentData )
             end
         end
     end
-    return results
+
+    unless !results.nil?
+        return results
+    else
+        msgDbg("Data unchanged")
+        exit 0
+    end
 end
 
 # }}}
@@ -206,7 +185,8 @@ end
 # {{{ Alert generation
 
 def alertDispatcher( results )
-    enabled_modules = getParameter('enabled_modules')
+
+    enabled_modules = $config[:enabled_modules]
 
     if enabled_modules.match( /.+,.+/ ) then
         enabled_modules.split(/,/, 2).each do |mod|
@@ -218,9 +198,9 @@ def alertDispatcher( results )
 end
 
 def doAlert( mod, results )
-    message_format = getParameter('message_format')
-    time_format = getParameter('time_format')
-    subject_format = getParameter('subject_format')
+    message_format = $config[:message_format]
+    time_format = $config[:time_format]
+    subject_format = $config[:subject_format]
 
     output = Array.new
     results.each do |result|
@@ -245,7 +225,7 @@ def doAlert( mod, results )
             pp out
         end
     when 'mysql':
-        table = getParameter("mysql_table")
+        table = $config[:mysql_table]
         sqlstring = "INSERT `#{table}` (time, hostnode, veid, parameter, oldvalue, currentvalue) values "
 
         results.each do |out|
@@ -258,6 +238,8 @@ def doAlert( mod, results )
             sqlstring += out[4].to_s + "'), "
         end
         sqlstring = sqlstring[0..-3]
+
+        msgDbg( sqlstring )
 
         dbh = connectSql()
         if dbh
@@ -272,14 +254,11 @@ def doAlert( mod, results )
             end
         end
     when 'email':
-        mail_from = getParameter('mail_from')
-        mail_to = getParameter('mail_to')
-
         output.each do |out|
             mail = TMail::Mail.new
             mail.date = Time.now
-            mail.from = mail_from
-            mail.to = mail_to
+            mail.from = $config[:mail_from]
+            mail.to = $config[:mail_to]
             mail.subject = out[0]
             mail.mime_version = "1.0"
             mail.set_content_type 'multipart', 'mixed'
@@ -310,9 +289,9 @@ end
 # alertDispatcher(results) -> doAlert()
 # writeFile(state, currentdata)
 
-CONFIG = getConfig( CONFIGFILE )
+getConfig( $config[:configfile] )
 
-oldData = validateData( readFile( STATEFILE ) )
+oldData = validateData( readFile( $config[:statefile] ) )
 
 currentData = getResource( getProcPaths() )
 
